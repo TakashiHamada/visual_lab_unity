8e7260b9d102d304044ee815e535bb38db8d96e8
TakashiHamada <hamada@gift10.net>
2015-07-12 19:01:52 +0900

    タイル生成を作成　タイルの挙動を

---
+++ Study00/Assets/scripts/GhostSkin.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GhostSkin : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Renderer>().material.color = new Color(0f,0f,0f,1f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		WeakenSkin ();
+	}
+	
+	void OnCollisionStay () {
+		StrongerSkin ();
+	}
+	
+	private void StrongerSkin() {
+		if(GetComponent<Renderer>().material.color.a > 1f) return;
+		GetComponent<Renderer>().material.color += new Color(0f,0f,0f,0.1f);
+	}
+	
+	private void WeakenSkin() {
+		if(GetComponent<Renderer>().material.color.a < 0f) return;
+		GetComponent<Renderer>().material.color -= new Color(0f,0f,0f,0.01f);
+	}
+}
---
+++ Study00/Assets/scripts/PaveTiles.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PaveTiles : MonoBehaviour {
+	
+	public GameObject paveObject;
+	
+	private float xSize = 10;
+	private float zSize = 5;
+	private float interval = 1f;
+
+	// Use this for initialization
+	void Start () {
+		
+		pave ();
+	}
+	
+	private void pave () {
+		
+		float xHalf = xSize / 2f;
+		float zHalf = zSize / 2f;
+		
+		for(float x = -xHalf; x < xHalf; x+= interval) 
+			for(float z = -zHalf; z < zHalf; z+= interval)
+				createTile (x, z);
+	}
+	
+	private void createTile (float x,float z) {
+		Instantiate(paveObject, new Vector3(x,0f,z), transform.rotation);
+	}
+}
