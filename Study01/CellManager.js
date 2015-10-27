c13c13d1987be28e31fad5ee7b65677b1273a58e
TakashiHamada <hamada@gift10.net>
2015-10-27 11:38:25 +0900

    コードを綺麗に。

--- Study01/CellManager.js
+++ Study01/CellManager.js
@@ -1,26 +1,7 @@
 var CellManager = function (row, column) {
- 
     var cell_width = P.width / column;
     var cell_height = P.height / row;
-    
-    var cells = new Array();
-    
-    for(var r = 0; r < row; r++){
-        for(var c = 0; c < column; c++){
-//            var cell = new Cell(c * cell_width, r * cell_height);
-            var cell = new Flower(c * cell_width, r * cell_height);
-            
-            
-            cells.push(cell);
-        }
-    }
-    
-    _.each(cells, function(cell){
-        
-//        cell.showBody();
-//        cell.showLinks(cells);
-        
-    });
-    
-    
+    for(var r = 0; r < row; r++)
+        for(var c = 0; c < column; c++)
+            new Flower(c * cell_width, r * cell_height);
 }
--- Study01/set_up.js
+++ Study01/set_up.js
@@ -1,57 +1,15 @@
 var P = null;
-
 function sketchProc(prosessing) {
-
     P = prosessing;
-
     P.setup = function () {
         P.colorMode(P.HSB, 360, 100, 100, 1);
         createFiled();
     }
-
-
     function createFiled() {
-        //        P.background(P.color(0));
         P.background(P.color(4, 90, 90));
-        //        P.background(H.palette());
         P.size(1200, 800);
-
-        //        new PixelManager(150);
-                new CellManager(36,29);
-
-        //        var row = 5;
-        //        var column = 5;
-        //
-        //        var cell_width = P.width / column;
-        //        var cell_height = P.height / row;
-        //
-        //        for (var r = 0; r < row; r++) {
-        //            for (var c = 0; c < column; c++) {
-        //
-        //                new Ougi(cell_width * r, cell_width * c);
-        //            }
-        //        }
-
-//        _.times(6, function (index) {
-//
-//
-//            P.fill = P.noFill();
-//            P.stroke(P.color(0, 50, 100));
-//            P.strokeWeight(20);
-//            P.ellipse(200, 300 - (index * 40), 200, 50);
-//
-//            P.strokeWeight(4);
-//            P.stroke(P.color(0, 20, 100));
-//            P.ellipse(200, 290- (index * 40), 210, 50);
-//
-//        });
-
-
-//        new Flower(200,200);
-
-
+        new CellManager(36,29);
     }
 }
-
 var canvas = document.getElementById("stage");
 var processingInstance = new Processing(canvas, sketchProc);
